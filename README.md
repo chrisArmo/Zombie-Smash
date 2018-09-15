ba2b6192246eed8918a20761d35d58769735671f
Chris Armendarez <chris.michael.armo@gmail.com>
2018-09-15 11:07:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Zombie Smash
+
+Zombie Smash is a Node JS command-line game where a player tries to successfully land damage inflicting blows to the Zombie.
+If the player's attack doesn't land, the player receives damage.
+
+## Use
+
+The player chooses between the Zombie's head, chest, arms, and legs to try and inflict damage.
+One of the locations is the going to be vulnerable at random. The player will only land if they choose the vulnerable location.
+
+## Tips
+
+Depending on the body part the player targets, missing is more costly.
+
+### Info
+
+To reset the game scoreboard, enter "reset" or "r" as an argument when using Node to run the file.
---
+++ zombie.js
@@ -0,0 +1,152 @@
+/**
+ * Zombie Game
+ */
+
+// Dependencies
+// ==================================================
+
+const inquirer = require("inquirer"),
+fs = require("fs"),
+readline = require("readline");
+
+// Global Variables
+// ==================================================
+
+// Argument for reset check
+const [nodePath, filePath, resetArgument] = process.argv,
+// Game tracker
+gameTracker = {
+    player: 70,
+    zombie: 20,
+    over: false,
+    score: {},
+    locations: [
+        "head", "chest", "legs", "arms"
+    ],
+    damage: {
+        head: 10,
+        chest: 6,
+        legs: 4,
+        arms: 3
+    }
+};
+
+
+// Functions
+// ==================================================
+
+// Get random number
+const getRandomNumber = (max = 4) => Math.floor((Math.random() * max));
+
+// Check game reset
+const checkReset = (reset) => {
+    const resetPattern = /^(reset|r)$/;
+    return resetPattern.test(reset);
+};
+
+// Clear console
+const clearConsole = () => {
+    console.log("\n".repeat(process.stdout.rows));
+    readline.cursorTo(process.stdout, 0, 0);
+    readline.clearScreenDown(process.stdout);
+};
+
+// Play game
+const playGame = (game) => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "target",
+            message: "Try to stay alive! \nSwing your bat at what part of the zombie?",
+            choices: ["head", "chest", "legs", "arms"]
+        }
+    ]).then((actions) => {
+        if (!game.over) {
+            fs.readFile("scoreboard.json", "utf8", (err, data) => {
+                if (!err) {
+                    const zombieVulnerability = game.locations[getRandomNumber(game.locations.length)], 
+                    damage = game.damage[actions.target],
+                    score = JSON.parse(data);
+
+                    clearConsole();
+
+                    game.score.player = parseInt(score.player, 10);
+                    game.score.zombie = parseInt(score.zombie, 10);
+
+                    console.log(`Scoreboard: Player ${score.player} | Zombie ${score.zombie}`);
+        
+                    if (actions.target === zombieVulnerability) {
+                        game.zombie -= damage;
+                        console.log(`\nDirect hit with the zombie's ${actions.target}`);
+                        console.log(`You dealt the zombie ${damage} damage.`);
+                    } else {
+                        game.player -= damage;
+                        console.log(`\nYou missed the zombie's ${actions.target}`);
+                        console.log(`The Zombie dealt you ${damage} damage.`);
+                    }
+        
+                    if (game.player > 0 && game.zombie > 0) {
+                        console.log(
+                            `\nYou have ${game.player} health remaining` +
+                            `\nThe zombie has ${game.zombie} health remaining\n`
+                        );
+                    }
+                    
+                    checkGame(game);
+                }
+            });
+        }
+    });
+};
+
+// Check game
+const checkGame = (game) => {
+    if (game.player <= 0) {
+        game.score.zombie += 1;
+        game.over = true;
+        console.log("\nBummer, the zombie devoured your brain");
+    } else if (game.zombie <= 0) {
+        game.score.player += 1;
+        game.over = true;
+        console.log("\nYou have vanquished the zombie!");
+    }
+
+    if (!game.over) {
+        playGame(game);
+    } else {
+        fs.writeFile(
+            "scoreboard.json", 
+            JSON.stringify({
+                player: game.score.player,
+                zombie: game.score.zombie
+            }), 
+            (err) => {
+                if (!err) {
+                    console.log(`\nEnd Game Scoreboard: Player ${game.score.player} | Zombie ${game.score.zombie}`);
+                }
+            }
+        );
+    }
+};
+
+// Main
+// ==================================================
+
+if (checkReset(resetArgument)) {
+    fs.writeFile(
+        "scoreboard.json",
+        JSON.stringify({
+            player: 0,
+            zombie: 0
+        }),
+        (err) => {
+            if (!err) {
+                clearConsole();
+                playGame(gameTracker);
+            }
+        }
+    );
+} else {
+    clearConsole();
+    playGame(gameTracker);
+}
